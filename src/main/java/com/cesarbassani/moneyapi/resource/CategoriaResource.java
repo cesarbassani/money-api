e501a264784dd2e5533514906d608bcbb1204d59
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2019-06-20 18:52:57 -0400

    Listando todas as Categorias

--- src/main/java/com/cesarbassani/moneyapi/resource/CategoriaResource.java
+++ src/main/java/com/cesarbassani/moneyapi/resource/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.cesarbassani.moneyapi.resource;
 import com.cesarbassani.moneyapi.model.Categoria;
 import com.cesarbassani.moneyapi.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
