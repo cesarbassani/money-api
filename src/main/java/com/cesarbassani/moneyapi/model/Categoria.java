c7080573e71c6b03e9ced2bfd0255c85b7968bdb
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2019-06-24 11:17:49 -0400

    Validando valores inválidos com Bean Validation

--- src/main/java/com/cesarbassani/moneyapi/model/Categoria.java
+++ src/main/java/com/cesarbassani/moneyapi/model/Categoria.java
@@ -2,11 +2,15 @@ package com.cesarbassani.moneyapi.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "categoria")
 public class Categoria extends EntidadeBase {
 
+    @NotNull
+    @Size(min = 3, max = 20)
     private String nome;
 
     public String getNome() {
